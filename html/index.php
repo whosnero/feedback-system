fa58abbd6946f255ee7852cf453e3f419efb5e53
revays <Kilian.schlaefereit@gmail.com>
2021-10-09 14:02:09 +0200

    added bootstrap css & JS

---
+++ html/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Stylesheet 1-->
+	<link rel="stylesheet" href="css/stylesheet1.css"></link>
+    <!-- Bootstrap CSS file -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <title>Feedback-System</title>
+</head>
+<body>
+    <article>
+        
+    </article>
+    <footer>
+    
+    </footer>
+    <!-- JS files: jQuery first, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
