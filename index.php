1ce54a139b92395e76d0b03b1216dc48889e4cf7
nero <39500180+whosnero@users.noreply.github.com>
2021-10-11 12:29:13 +0200

    test

--- index.php
+++ index.php
@@ -27,13 +27,15 @@
             <div class="container-fluid">
                 <div id="mainrow" class="row">
                     <div id="mainfield" class="col-md-12">
-                        <h1>Willkommen Kilian!</h1>
+                        <h1>Willkommen Tobias!</h1>
                         <p>Umfrage erstellen</p>
                     </div>
                 </div>
             </div>
     </article>
     <footer>
+
+    <!--sfa --> 
     
     </footer>
     <!-- JS files: jQuery first, then Bootstrap JS -->
