01e857c2840fd3f4a109fefc4f82b4800f70e06a
nero <39500180+whosnero@users.noreply.github.com>
2022-01-01 23:00:06 +0100

    auto create (db)

--- db.sql
+++ db.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `feedo`
 --
+CREATE DATABASE IF NOT EXISTS `feedo` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `feedo`;
 
 -- --------------------------------------------------------
 
